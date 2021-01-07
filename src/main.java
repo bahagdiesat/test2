bdc64030d1c67aad3087db3436e6b0e81e97bf2f
baha <bahapda@hotmail.com>
2021-01-07 23:40:52 +0200

    v1

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println(max(5,2));
+    }
+
+    public static int max(int x, int y){
+        return x;
+    }
+}
